f38ed262d55408bd2e128af37283c1b691926250
Dominik <dominiklud1120@gmail.com>
2024-04-18 13:08:33 +0200

    hihiha

---
+++ Merokz/Helpers/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCC.Helpers
+{
+    public class UnitTest
+    {
+        public int ID { get; set; }
+        public string[] Input { get; set; }
+        //public (int, int) CaseDimensions { get; set; }
+
+        //publci string[] Cases { get; set; }
+        public int CasesAmount { get; set; }
+        public string[] Output { get; set; }
+
+        public UnitTest(int unitTestID, string[] unitTestinput)
+        {
+            ID = unitTestID;
+            Input = unitTestinput;
+        }
+    }
+}
---
+++ Merokz/Level4.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using CCC.Helpers;
+
+namespace CCC
+{
+    public class Level4 : ILevel
+    {
+        public int Level { get; set; }
+        public bool Debug { get; set; }
+        public string[,] LevelFiles { get; set; }
+        private int CurrentUnitTest { get; set; }
+        Dictionary<int, UnitTest> UnitTests { get; set; } = new Dictionary<int, UnitTest>();
+
+        public Level4()
+        {
+        }
+
+        public void Run()
+        {
+            for (CurrentUnitTest = 0; CurrentUnitTest <= LevelFiles.Length / 3 - 1; CurrentUnitTest++)
+            {
+                UnitTest unitTest = new UnitTest(CurrentUnitTest, LevelHelper.ReadUnitTestLines(LevelFiles[CurrentUnitTest, 0]));
+                UnitTests.TryAdd(CurrentUnitTest, unitTest);
+                RunUnitTest(unitTest);
+            }
+        }
+
+        public void RunUnitTest(UnitTest unitTest)
+        {
+            string[] Lines = unitTest.Input;
+
+
+
+            if (Debug) DebugUnitTest(unitTest);
+        }
+
+
+
+        public void DebugUnitTest(UnitTest unitTest)
+        {
+            List<string> DebugFile = ["DEBUGGING UNIT TEST: " + unitTest.ID];
+            
+            LevelHelper.WriteUnitTestLines(LevelFiles[unitTest.ID, 2], DebugFile.ToArray());
+        }
+    }
+}
